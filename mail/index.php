517ac8c3fad2095bffdab171745a9bd2ecf02c78
fvn-tiendv <fvn.tiendv@gmail.com>
2017-05-30 17:18:20 +0700

    up

--- mail/index.php
+++ mail/index.php
@@ -1,39 +1,14 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Send email via Gmail SMTP server in PHP</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <title>TTG Lancaster</title>
     </head>
     <body>
-        <div id="main">
-            <h1>Send Email via Gmail SMTP Server in PHP</h1>
-            <div id="login">
-                <h2>Send Email</h2>
-                <hr/>
-                <form action="index.php" method="post">
-                    <input type="text" placeholder="Họ và tên" name="ten"/>
-                    <input type="text" placeholder="0909151109" name="sdt"/>
-                    <input type="text" placeholder="Email" name="email"/>  
-                    <input type="text" placeholder="Tiêu đề: " name="subject"/>
-                    <textarea rows="4" cols="50" placeholder="Nội dung..." name="message"></textarea>
-                    <input type="submit" value="Send" name="send"/>
-                </form>    
-            </div>
-        </div>
           <?php
-        
               require 'phpmailer/PHPMailerAutoload.php';
-              
               if(isset($_POST['send']))
                   {
-
-                    // $to_id = $_POST['toid'];
                     $message = $_POST['message'];
                     $subject = $_POST['subject'];
                     $ten = $_POST['ten'];
@@ -72,17 +47,18 @@ and open the template in the editor.
 						IP:  '.$_SERVER['REMOTE_ADDR'].'<br />
 						';
 
-
-                    // $mail->msgHTML($message);
-
                     if (!$mail->send()) {
                        $error = "BẠN VUI LÒNG NHẬP ĐẦY ĐỦ CÁC TRƯỜNG! " . $mail->ErrorInfo;
                         ?><script>alert('<?php echo $error ?>');</script><?php
                     } 
                     else {
                        echo '<script>alert("YÊU CẦU CỦA BẠN ĐÃ ĐƯỢC GỬI ĐI, CHÚNG TÔI SẼ LIÊN HỆ VỚI BẠN!");</script>';
+                       $site_url = '../index.html';
+                       exit(header('Location: '.$site_url));
                     }
                }
         ?>
-    </body>
+ </body>
 </html>
+
+
